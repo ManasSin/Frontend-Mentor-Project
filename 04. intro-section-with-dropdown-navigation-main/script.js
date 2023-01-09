fc6744df01555301a28935c39e14816b6416e457
ManasSin <maltisadan12@gmail.com>
2023-01-09 23:50:16 +0530

    done 04 only js part left

---
+++ 04. intro-section-with-dropdown-navigation-main/script.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const floatingEelement = document.getElementsByClassName(
+  ".floating-list-features"
+);
+// floatingEelement.style.display = "block";
+
+console.log(floatingEelement);
