6e831d4a0f6a89b1b8733b869f44a58b11bd7760
ManasSin <maltisadan12@gmail.com>
2023-01-10 23:03:46 +0530

    little chagne which was need

--- 04. intro-section-with-dropdown-navigation-main/script.js
+++ 04. intro-section-with-dropdown-navigation-main/script.js
@@ -1,8 +1,29 @@
 "use strict";
 
-const floatingEelement = document.getElementsByClassName(
-  ".floating-list-features"
-);
-// floatingEelement.style.display = "block";
+const floating = document.querySelector(".floating-list-features");
 
-console.log(floatingEelement);
+const drop = document.querySelector(".drop-up");
+const dropdowns = document.querySelector(".dropdowns");
+
+console.log(floating);
+
+const hideShow = () => {
+  if (
+    floating.classList.contains("hidden") &&
+    drop.classList.contains("hidden")
+  ) {
+    floating.classList.remove("hidden");
+    drop.classList.remove("hidden");
+    dropdowns.classList.add("hidden");
+  } else {
+    floating.classList.add("hidden");
+    drop.classList.add("hidden");
+    dropdowns.classList.remove("hidden");
+  }
+};
+
+for (let i = 0; i < dropdowns.length; i++)
+  dropdowns[i].addEventListener("click", hideShow);
+
+for (let i = 0; i < drop.length; i++)
+  drop[i].addEventListener("click", hideShow);
