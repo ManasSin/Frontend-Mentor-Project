6d596e5b5369d24d7b53c9906b45ea10ee666194
ManasSin <maltisadan12@gmail.com>
2023-01-15 23:13:14 +0530

    done with css and JS Part Stared

---
+++ 05. time-tracking-dashboard-main/script.js
@@ -0,0 +1,10 @@
+const displayCards = [
+  {
+    backgroundImage: `./image/icon-${title.lowercase()}`,
+    title: "Work",
+    elipsis: "./image/icon-ellipsis.svg",
+    hours: "32hrs",
+    work: `Last Week - ${hours}`,
+    backgroundColor: "",
+  },
+];
